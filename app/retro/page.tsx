646b1a7b4482fd7d3a7e64ceb4f01047f7fb3e31
Yuurij-dev <yurijesus.dev@gmail.com>
2026-05-24 15:31:58 -0300

    feat: Breadcrumb adicionado na pagina retro

--- app/retro/page.tsx
+++ app/retro/page.tsx
@@ -9,6 +9,7 @@ import StoreHighlights from "../components/StoreHighlights";
 import NewsletterSection from "../components/NewsletterSection";
 import InstagramSection from "../components/InstagramSection";
 import Footer from "../components/Footer";
+import Link from "next/link";
 
 const Retro = () => {
     return ( 
@@ -16,6 +17,30 @@ const Retro = () => {
             <Header/>
             <main>
                 <section className="container !mx-auto !px-4 !py-8 sm:!px-6 lg:!px-0">
+
+                    <div className="!mb-6">
+                        {/* BREADCRUMB */}
+                        <div className="!mb-4 flex items-center gap-2 text-sm text-zinc-500">
+                        
+                            <Link
+                                href="/"
+                                className="transition-all duration-200 hover:text-black"
+                            >
+                                Início
+                            </Link>
+
+                            <span>›</span>
+
+                            <Link
+                                href="/retro"
+                                className="transition-all duration-200 hover:text-black"
+                            >
+                                Retro
+                            </Link>
+
+                        </div>
+                    </div>
+
                     <Banner image={bannerRetro} title={`MAIS QUE\nUMA CAMISA,\nUMA HISTÓRIA.`} description="Camisas dos maiores times do mundo com qualidade premium."/>
                     <RetroProductsGrid/>
                     <HistoricMoments/>
